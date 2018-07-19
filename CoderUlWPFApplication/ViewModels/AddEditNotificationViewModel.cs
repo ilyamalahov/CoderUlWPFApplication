a556a73d53a4bfc20298c1f8b98d8a4985d81fc4
Developer Rusdev <rusnetdeveloper@mail.ru>
2018-07-19 15:49:15 +0400

    201807191549

---
+++ CoderUlWPFApplication/ViewModels/AddEditNotificationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CoderUlWPFApplication.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderUlWPFApplication.ViewModels
+{
+    public class AddEditNotificationViewModel : BindableBase
+    {
+        public List<NotificationType> NotificationTypes { get; set; }
+
+        public List<Location> Locations { get; set; }
+
+        public Notification CreateNotification()
+        {
+            return new Notification
+            {
+                Message = "",
+                NotificationTypeId = 0
+            };
+        }
+    }
+}
--- CoderUlWPFApplication/ViewModels/ShellViewModel.cs
+++ CoderUlWPFApplication/ViewModels/ShellViewModel.cs
@@ -19,11 +19,82 @@ namespace CoderUlWPFApplication.ViewModels
         {
             dbContext = databaseContext;
 
-            var notifications = dbContext.Notifications.Include(n => n.NotificationType).ToList();
+            var notifications = dbContext.Notifications
+                .Include(n => n.NotificationType)
+                .ToList();
 
             Notifications = new ObservableCollection<Notification>(notifications);
 
             QuitCommand = new DelegateCommand(QuitApplication, () => { return true; });
+            ChangeScreenModeCommand = new DelegateCommand(() => { }, () => { return true; });
+
+            PreviousCommand = new DelegateCommand(GoPrevious, CanGoPrevious);
+            NextCommand = new DelegateCommand(GoNext, CanGoNext);
+
+            PrintCommand = new DelegateCommand(PrintNotification, CanPrintNotification);
+
+            AddCommand = new DelegateCommand(AddNotification, CanAddNotification);
+            DeleteCommand = new DelegateCommand(DeleteNotification, CanDeleteNotification);
+            EditCommand = new DelegateCommand(EditNotification, CanEditNotification);
+        }
+
+        private bool CanEditNotification()
+        {
+            return false;
+        }
+
+        private void EditNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanDeleteNotification()
+        {
+            return false;
+        }
+
+        private void DeleteNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanAddNotification()
+        {
+            return true;
+        }
+
+        private void AddNotification()
+        {
+        }
+
+        private bool CanPrintNotification()
+        {
+            return false;
+        }
+
+        private void PrintNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanGoNext()
+        {
+            return false;
+        }
+
+        private void GoNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanGoPrevious()
+        {
+            return false;
+        }
+
+        private void GoPrevious()
+        {
+            throw new NotImplementedException();
         }
 
         private void QuitApplication()
@@ -32,6 +103,16 @@ namespace CoderUlWPFApplication.ViewModels
         }
 
         public DelegateCommand QuitCommand { get; private set; }
+        public DelegateCommand ChangeScreenModeCommand { get; private set; }
+
+        public DelegateCommand PreviousCommand { get; private set; }
+        public DelegateCommand NextCommand { get; private set; }
+
+        public DelegateCommand PrintCommand { get; private set; }
+
+        public DelegateCommand DeleteCommand { get; private set; }
+        public DelegateCommand AddCommand { get; private set; }
+        public DelegateCommand EditCommand { get; private set; }
 
         private readonly DatabaseContext dbContext;
 
